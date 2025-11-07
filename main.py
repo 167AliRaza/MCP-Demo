537a0460ed0483652dd4ee9579cc26c319319563
Ali Raza <167aliraz@gmail.com>
2025-11-08 00:13:32 +0500

    main updated

--- main.py
+++ main.py
@@ -287,6 +287,6 @@ app.mount("/mcp", mcp.sse_app())
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
